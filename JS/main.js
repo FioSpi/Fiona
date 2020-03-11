6cc0efded6bc4ccb9ee37f2123f337a1b142c0f3
FioSpi <fiona.57@hotmail.fr>
2020-03-11 15:47:46 +0100

    11/03/2020

--- JS/main.js
+++ JS/main.js
@@ -2,5 +2,10 @@ document.getElementById('image_menu').onclick = function() {
     document.getElementById('main_nav').classList.toggle('show');
 }
 
+$(document).ready(function (){
+    $('').click(function(){
 
+    });
+
+});
 
